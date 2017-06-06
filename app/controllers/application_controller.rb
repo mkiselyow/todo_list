eb279bd7c61377413bab9f28ca107452394a23e6
MaximKiselyow <m.kiselyow@gmail.com>
2017-06-06 03:53:37 +0300

    correct_user_some_other_fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,8 @@ class ApplicationController < ActionController::Base
       @current_user ||= User.find(session[:user_id]) if session[:user_id]
     end
     helper_method :current_user
+
+    def authorized
+      redirect_to root_url if current_user
+    end
 end
